97326f476b57c27c67fe85df8a81e13e51f1634a
Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-01-11 23:11:40 +1100

    fixed the status script

--- create_status_report.sh
+++ create_status_report.sh
@@ -3,12 +3,12 @@
 cat $1 \
   | jq -r '.[] | [.location, .result.ajaxresult.slots.nextAvailableDate] | @csv' \
   | Rscript -e 'suppressMessages(library(tidyverse)); 
+                suppressMessages(library(jsonlite));
                 read_csv(file("stdin"),
                          col_names = c("location","next_available_time"), 
                          col_types = cols()) %>% 
-                  left_join(read_csv("centres.csv", 
-                                     col_names=c("location","name"),
-                                     col_types = cols()),
+                  left_join(fromJSON("./docs/centers.json") %>%
+                              select(location = id, name),
                             by = "location") %>% 
                   select(name, next_available_time) %>% 
                   format_csv() %>% cat();'
