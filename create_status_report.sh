34642eb0e3c1c479b97dc7873266a280c46f2c78
Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-01-12 12:37:41 +1100

    removed R dependency

--- create_status_report.sh
+++ create_status_report.sh
@@ -1,14 +1,6 @@
 #! /bin/bash
 
-cat $1 \
-  | jq -r '.[] | [.location, .result.ajaxresult.slots.nextAvailableDate] | @csv' \
-  | Rscript -e 'suppressMessages(library(tidyverse)); 
-                suppressMessages(library(jsonlite));
-                read_csv(file("stdin"),
-                         col_names = c("location","next_available_time"), 
-                         col_types = cols()) %>% 
-                  left_join(fromJSON("./docs/centers.json") %>%
-                              select(location = id, name),
-                            by = "location") %>% 
-                  select(name, next_available_time) %>% 
-                  format_csv() %>% cat();'
+join -a 1 -t, \
+  <(jq -r '.[] | [(.id | tojson),.name] | @csv' $1 | sort -k 1) \
+  <(jq -r '.[]| [.location, .result.ajaxresult.slots.nextAvailableDate] | @csv' $2 | sort -k 1) \
+  | sort -t, -k 2
