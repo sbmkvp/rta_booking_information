00d5b910614cc8a7d22d5ef82842b6a7cee64808
Dr Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-01-27 07:49:45 +1100

    added logging via log file

--- get_all_locations.sh
+++ get_all_locations.sh
@@ -3,9 +3,12 @@
 git_upload="$(jq '.git_upload' settings.json)"
 python3_exe="$(jq '.python3_executable' settings.json)"
 
+echo "$(date +'[%T] :') Pulling latest information from gihub repository..."
+git pull
 
+echo "$(date +'[%T] :') Starting the scraping process..."
 jq '.[].id' docs/centers.json \
-  | parallel "$python3_exe scrape_availability.py {} results.json || echo {} >> errors.txt"
+  | parallel "$python3_exe scrape_availability.py {} results.json &> logs.txt || echo {} >> errors.txt"
 
 ERR_NUM="$(wc -l < errors.txt)"
 
@@ -13,18 +16,23 @@ while [[ $ERR_NUM -gt 0 ]]
 do 
   mv errors.txt errors_old.txt
   cat errors_old.txt \
-    | parallel "$python3_exe ./scrape_availability.py {} results.json || echo {} >> errors.txt" 
+    | parallel "$python3_exe ./scrape_availability.py {} results.json &> logs.txt || echo {} >> errors.txt" 
   ERR_NUM="$(wc -l < errors.txt)"
 done 
+echo "$(date +'[%T] :') Scraping process completed..."
 
+echo "$(date +'[%T] :') Reorganising results..."
 jq -s . results.json > ./docs/results.json &&
 echo "$(date +'%Y-%m-%d %H:%M%p')" > ./docs/update-time.txt && 
 rm results.json errors_old.txt
 
 if [[ $git_upload == 'true' ]];
   then
+    echo "$(date +'[%T] :') Pushing the latest data update back to github..."
     git pull &&
     	git add ./docs/results.json ./docs/update-time.txt && 
     	git commit -m "data update" && 
-    	git push
+    	git push --quiet
 fi
+
+echo "$(date +'[%T] :') All processes complete."
