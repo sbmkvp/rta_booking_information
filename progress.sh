5b022216a9e4ea19d10f4e93d78dbaf9407f3eac
Dr Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-03-15 22:14:15 +1100

    added script to show progress in tmux window

---
+++ progress.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+WIDTH="$(tput cols)"
+MAX="$(($(wc -l docs/centers.json | awk '{print $1}')-2))"
+CURRENT="$(wc -l results.json | awk '{print $1}')"
+REPS_F="$(echo $WIDTH/$MAX*$CURRENT | bc -l)"
+REPS=$(printf '%.0f' $REPS_F)
+for i in $(seq 1 $REPS); do printf '='; done;
+echo ''
