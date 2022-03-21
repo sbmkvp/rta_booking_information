c8f50ba2c601cf9683d45d3f3b0640083e3d5c93
Dr Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-03-21 19:32:34 +1100

    formattig progress script

--- progress.sh
+++ progress.sh
@@ -5,5 +5,6 @@ MAX="$(($(wc -l docs/centers.json | awk '{print $1}')-2))"
 CURRENT="$(wc -l results.json | awk '{print $1}')"
 REPS_F="$(echo $WIDTH/$MAX*$CURRENT | bc -l)"
 REPS=$(printf '%.0f' $REPS_F)
+echo ''
 for i in $(seq 1 $REPS); do printf '='; done;
 echo ''
