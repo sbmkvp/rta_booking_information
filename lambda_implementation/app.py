86baf7f1e90fbab04a261be244ba0e5fb6663edb
Balamurugan Soundararaj <sbm.kvp@gmail.com>
2022-05-27 04:25:27 +1000

    lambda implementation started

---
+++ lambda_implementation/app.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from tempfile import mkdtemp
+
+
+def handler(event=None, context=None):
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+    chrome = webdriver.Chrome("/opt/chromedriver", options=options)
+    chrome.get("https://www.myrta.com/wps/portal/extvp/myrta/login/")
+    return chrome.find_element_by_xpath("//html").text
